eda29ccd599525f64716e22fac8cd7419accd216
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-09-20 09:43:19 +0300

    get the api, showcase the track name

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+
 
 export default function App() {
+  const [radioData, setRadioData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://complex.in.ua/status-json.xsl?mount=/yantarne')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setRadioData(data);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>{radioData.icestats.source.title}</Text>
       <StatusBar style="auto" />
     </View>
   );
